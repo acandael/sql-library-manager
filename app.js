eac11ce4157519abe347554e42b1729907ba922c
acandael <info@anthonycandaele.com>
2019-03-23 11:27:33 +0100

    finish project 8

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Import routes
+const indexRoute = require('./routes');
+const booksRoute = require('./routes/books');
+const newBookRoute = require('./routes/new-book');
+const updateBookRoute = require('./routes/update-book');
+const deleteBookRoute = require('./routes/delete-book');
+
+// Port
+const port = process.env.PORT || 3000;
+
+// Create Express app
 const app = express();
 
-module.exports = app;
+// Add body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set view engine to Pug
+app.set('view engine', 'pug');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use(indexRoute);
+app.use('/books', booksRoute);
+app.use('/books/new', newBookRoute);
+app.use('/books/:id', updateBookRoute);
+app.use('/books/:id/delete', deleteBookRoute);
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).render('page-not-found');
+});
+
+// Handle 500 errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('error');
+});
+
+// Create server
+app.listen(port, () => console.log(`App is listening to port ${port}`));
---
+++ routes/new-book.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const Book = require('../models').Book;
+
+// Create new router
+const router = express.Router();
+
+// GET show the create new book form
+router.get('/', (req, res) => {
+  res.render('new-book');
+});
+
+// POST add a new book to the database
+router.post('/', (req, res) => {
+  Book.create(req.body)
+    .then(book => {
+      res.redirect(`/books/${book.id}`);
+    })
+    .catch(err => {
+      if (err.name === 'SequelizeValidationError') {
+        res.render('new-book', {
+          book: Book.build(req.body),
+          errors: err.errors
+        });
+      } else {
+        throw err;
+      }
+    })
+    .catch(err => {
+      res.sendStatus(500);
+    });
+});
+
+// Export router
+module.exports = router;
